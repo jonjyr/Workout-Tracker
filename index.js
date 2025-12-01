f0379afa284778d157eed197a716ae6eeacea07f
jonjyr <159262148+jonjyr@users.noreply.github.com>
2025-12-01 14:49:47 +0200

    Refactor for App.js complete. App now uses WorkoutTracker component for main view. Shifted GestureHandlerRootView wrapper from index.js to App.js.

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 import { registerRootComponent } from 'expo';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import App from './App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-function Root() {
-  return (
-    // FYI: Use GestureHandlerRootView around the App to enable gestures on Expo
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <App />
-    </GestureHandlerRootView>
-  );
-}
-
-registerRootComponent(Root);
+registerRootComponent(App);
